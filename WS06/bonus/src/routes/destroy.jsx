fb499aaa0d99d407bcd49d50261e134860dc70b9
roynykanen <ron00003@laurea.fi>
2025-05-08 15:59:41 +0300

    WS06 added

---
+++ WS06/bonus/src/routes/destroy.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import { redirect } from "react-router-dom";
+import { deleteContact } from "../contacts";
+
+export async function action({ params }) {
+  try {
+    await deleteContact(params.contactId);
+    return redirect("/");
+  } catch (error) {
+    throw new Error("oh dang!");
+  }
+}
